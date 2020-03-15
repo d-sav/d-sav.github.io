bf3f2092de37e7793607e7b22fd9e86dea876cae
jschrbr <schrbrr@gmail.com>
2020-03-15 15:15:31 +1100

    Bouncin

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,10 @@
 //   $(".sidenav").sidenav();
 // });
 
+$(".landing-img").addClass("scale-transition");
+$(".landing-img").addClass("scale-out");
 M.AutoInit();
+
+$("document").ready(function() {
+  $(".landing-img").removeClass("scale-out");
+});
